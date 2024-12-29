0117806b5fd9ea7cd9988754579e6c6b861c790d
Aseel <aseel@lektik.com>
2024-12-29 21:04:50 +0530

    feat : add unique id for eveery user

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,6 +1,8 @@
 import mongoose, { Schema } from "mongoose";
+import { Counter } from "./counter.model.js";
 
 const userSchema = new mongoose.Schema({
+  userId: { type: String, unique: true },
   name: { type: String, required: true },
   gender: { type: String, required: true, enum: ["male", "female"] },
   phoneNumber: { type: String, required: true, unique: true },
@@ -11,9 +13,21 @@ const userSchema = new mongoose.Schema({
   capId: { type: String, required: true },
   image: { type: String, required: true },
   dob : { type: Date, required: true },
-  total_score: { type: Number, default: 0 }, 
+  total_score: { type: Number, default: 0 }
 } , {
     timestamps: true,
+});
+
+userSchema.pre('save', async function(next) {
+  if (this.isNew) {
+    const counter = await Counter.findOneAndUpdate(
+      { _id: 'userId' },
+      { $inc: { seq: 1 } },
+      { new: true, upsert: true }
+    );
+    this.userId = `KLM${String(counter.seq).padStart(4, '0')}`;
+  }
+  next();
 });
 
 export const User = mongoose.model("User", userSchema);
