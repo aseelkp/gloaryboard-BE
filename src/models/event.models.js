24033ea9e9b323578a6d0bf74890983ce3e77fd7
Aseel <aseel@lektik.com>
2025-01-12 22:24:47 +0530

    hot fix : update requirement of start and endtime

--- src/models/event.models.js
+++ src/models/event.models.js
@@ -3,13 +3,12 @@ import mongoose, { Schema } from "mongoose";
 const eventSchema = new Schema({
   name: { type: String, required: true },
   event_type: { type: Schema.Types.ObjectId, ref: "EventType", required: true },
-  event_category: { type : String, required: true },
+  event_category: { type: String, required: true },
   result_category: { type: String },
   min_participants: { type: Number, required: true },
   max_participants: { type: Number, required: true },
-  start_time: { type: Date, required: true },
-  end_time: { type: Date, required: true },
+  start_time: { type: Date },
+  end_time: { type: Date },
 });
-
 
 export const Event = mongoose.model("Event", eventSchema);
