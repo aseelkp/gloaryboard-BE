f9947e92facfd4b9fa4bb7698212d49cc67aa826
Nahyan Sharvin <nahyansharvin@gmail.com>
2025-01-12 15:32:46 +0530

    feat: add config for zone A

--- src/utils/zoneConfig.js
+++ src/utils/zoneConfig.js
@@ -3,10 +3,15 @@ import { rgb } from 'pdf-lib';
 export const getZoneConfig = (zone) => {
     
     switch (zone.toLowerCase()) {
+      case 'a':
+        return {
+          primaryColor: rgb(0.69, 0.18, 0.51), 
+          headerImagePath: './src/templates/zone_a_participant_ticket_header.png'
+        };
       case 'c':
         return {
           primaryColor: rgb(0.52, 0.45, 0.19), 
-          headerImagePath: './src/templates/zone_a_participant_ticket_header.png'
+          headerImagePath: './src/templates/zone_c_participant_ticket_header.png'
         };
       case 'b':
         return {
