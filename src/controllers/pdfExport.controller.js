a22fe0a22a6c3b59fcc09184f5629153d43a5166
Nahyan Sharvin <nskonnalath@gmail.com>
2025-01-14 21:42:01 +0530

    feat: add controller to fetch program participants list

--- src/controllers/pdfExport.controller.js
+++ src/controllers/pdfExport.controller.js
@@ -141,39 +141,32 @@ const getParticipantTicketById = asyncHandler(async (req, res, next) => {
 const getProgramParticipantsListById = asyncHandler(async (req, res, next) => {
   const { id } = req.params;
   
-  const participants = [
-    { "name": "Alice Johnson", "college": "University of Kerala" },
-    { "name": "Bob Smith", "college": "Cochin University of Science and Technology" },
-    { "name": "Charlie Brown", "college": "Mahatma Gandhi University, Kottayam" },
-    { "name": "David Wilson", "college": "Calicut University" },
-    { "name": "Eve Davis", "college": "Kannur University" },
-    { "name": "Frank Thomas", "college": "Kerala Agricultural University" },
-    { "name": "Grace Lee", "college": "APJ Abdul Kalam Technological University" },
-    { "name": "Hannah White", "college": "St. Teresa's College, Ernakulam" },
-    { "name": "Isaac Martinez", "college": "Sacred Heart College, Thevara" },
-    { "name": "Jack Taylor", "college": "Farook College, Kozhikode" },
-    { "name": "Katie Moore", "college": "Mar Ivanios College, Thiruvananthapuram" },
-    { "name": "Liam Anderson", "college": "Christ College, Irinjalakuda" },
-    { "name": "Mia Thompson", "college": "Amrita Vishwa Vidyapeetham, Kollam" },
-    { "name": "Noah Martin", "college": "MES College, Marampally" },
-    { "name": "Olivia Clark", "college": "Government Victoria College, Palakkad" },
-    { "name": "Paul Lewis", "college": "St. Joseph's College, Devagiri" },
-    { "name": "Quincy Hall", "college": "Union Christian College, Aluva" },
-    { "name": "Rachel Walker", "college": "NSS College, Ottapalam" },
-    { "name": "Sam Harris", "college": "Govt. Brennen College, Thalassery" },
-    { "name": "Tina Young", "college": "St. Berchmans College, Changanassery" },
-    { "name": "Uma Patel", "college": "Maharaja's College, Ernakulam" },
-    { "name": "Victor King", "college": "CMS College, Kottayam" },
-    { "name": "Wendy Scott", "college": "Baselios College, Kottayam" },
-    { "name": "Xander Evans", "college": "Government College, Kasaragod" },
-    { "name": "Yara Green", "college": "SN College, Kollam" },
-    { "name": "Zachary Adams", "college": "Providence College, Kozhikode" }
-  ]
-  
+  const eventRegistrations = await EventRegistration.find({
+    event: id,
+  }).populate("participants.user", "name college").populate({
+    path: "event",
+    select: "name",
+    populate: {
+      path: "event_type",
+      select: "is_group is_onstage",
+    }
+  });  
 
+  if (eventRegistrations.length === 0) {
+    return next(new ApiError(404, "No registrations found for the specified event"));
+  }
+
+  const eventName = eventRegistrations[0].event.name;
+  const eventType = !eventRegistrations[0].event.event_type.is_onstage ? "Off Stage" : eventRegistrations[0].event.event_type.is_group ? "Group" : "Stage";
+
+  const participants = eventRegistrations.map((reg) => ({
+    name: reg.participants[0].user.name,
+    college: reg.participants[0].user.college
+  }));
+  
   const data = {
-    name: " THUKAL VADHYANGAL (EASTERN) TABALA / PAKKAVADHYAM",
-    type: "Stage",
+    name: eventName,
+    type: eventType,
     participants,
   };
 
